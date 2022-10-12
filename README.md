6098f93527fa87a726b0c48857e6bb98d8883248
lisaericksoncoding <111710705+lisaericksoncoding@users.noreply.github.com>
2022-10-11 23:33:21 -0500

    images, README, and updates to js

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
 # Weather_Dashboard
+
+<h1 align="center"> Weather Dasboard Challenge</h1>
+
+<p align="center">Create a simple calendar application that allows a user to save events for each hour of the day.</p>
+
+## Screenshots 
+<img src="./assets/Weather Dashboard.PNG"></img>
+<img src="./assets/Weather Data.PNG"></img>
+
+## Links
+
+- [Repo](https://github.com/lisaericksoncoding/Weather_Dashboard "Weather Dashboard Repo")
+
+- [Live](https://lisaericksoncoding.github.io/Weather_Dashboard "Live View")
+
+## Built With
+
+- HTML
+- CSS
+- Visual Studio Code
+- JQuery
+- Bootstrap
+- Moment
+
+## Resources
+
+- [W3Schools Site](https://www.w3schools.com "W3Schools")
+- [StackOverflow Site](https://stackoverflow.com/ "Stack Overflow")
+- [JavaScript.info Site](https://javascript.info/ "JavaScript.info")
+- [Moment.js Site](https://momentjs.com/ "Moment.js")
+- Bootcamp classmates
+
+## Author
+
+**Lisa Erickson**
+
+- [Profile](https://github.com/lisaericksoncoding "Lisa Erickson")
+- [LinkedIn](https://www.linkedin.com/in/lisalerickson/ "Lisa Erickson")
+- [Email](mailto:erickson.l.lisa@gmail.com?subject=Feedback "Feedback")
+
+## License
+MIT License 
+
+## 🤝 Support
+
+Feedback is welcome!
--- assets/script.js
+++ assets/script.js
@@ -4,8 +4,9 @@ var city = document.getElementById('inputCity');
 var APIKey = "385978d94fcfbfed39d6f1fa9a036cef";
 var h2 = document.getElementById("hide");
 
-function getApi() {
 
+function getApi() {
+    
     var requestUrl = "http://api.openweathermap.org/data/2.5/forecast?units=imperial&q=" + city.value + "&appid=" + APIKey;
 
 
@@ -16,7 +17,7 @@ function getApi() {
         .then(function (data) {
             console.log(data)
 
-            for (var i = 0; i < data.list.length; i+=8) {
+            for (var i = 0; i < data.list.length; i += 8) {
 
                 var cardInfo = document.createElement('div');
                 var cardDate = document.createElement('h3');
@@ -25,8 +26,9 @@ function getApi() {
                 var cardDescription = document.createElement('p');
                 var cardWind = document.createElement('p');
                 var cardHumid = document.createElement('p');
+                
 
-                h2.style.visibility = "visible";	
+                h2.style.visibility = "visible";
 
                 cardInfo.classList.add('cardInfo');
                 cardDate.classList.add('cardDate');
@@ -35,18 +37,35 @@ function getApi() {
                 cardDescription.classList.add('cardList');
                 cardWind.classList.add('cardWind');
                 cardHumid.classList.add('cardHumid');
+                
+
 
                 cardIcon.src = "http://openweathermap.org/img/wn/" + data.list[i].weather[0].icon + "@2x.png";
                 cardDate.textContent = moment(data.list[i].dt * 1000).format("dddd, MMMM Do YYYY");
-                cardTemp.innerHTML = data.list[i].main.temp + " <i class='fas fa-temperature-low'></i>";
-                cardDescription.innerHTML = data.list[i].weather[0].description;
-                cardWind.innerHTML = data.list[i].wind.speed + (" mph ") + " <i class='fas fa-wind'></i>";
-                cardHumid.innerHTML = data.list[i].main.humidity + (" % humidity ");
+                cardTemp.innerHTML = Math.floor(data.list[i].main.temp) + " <i class='fas fa-temperature-low'></i>";
+                cardDescription.innerHTML = ("Conditions: ") + data.list[i].weather[0].description;
+                cardWind.innerHTML = ("Wind Speed:") + Math.floor(data.list[i].wind.speed) + (" mph ") + " <i class='fas fa-wind'></i>";
+                cardHumid.innerHTML = Math.floor(data.list[i].main.humidity) + ("% humidity ");
+              
 
                 cardInfo.append(cardIcon, cardDate, cardTemp, cardDescription, cardWind, cardHumid);
                 weatherCards.appendChild(cardInfo);
             }
+            function createCityList() {
+                var cityListTotal = document.createElement('div');
+                var cityList = document.createElement('button');
+
+                cityListTotal.classList.add('cityListTotal');
+                cityList.classList.add('cityList');
+
+                cityList.textContent = city.value;
+                cityListTotal.append(cityList);
+            }
+            
         });
 }
 
 getWeatherBtn.addEventListener('click', getApi)
+
+
+
